6d37906a7b58d849ceefa40d5630fc206f8f3da8
DICKY WAHYU RAMADANI <ramadani.wahyu@gmail.com>
2024-07-11 20:48:31 +0700

    analisis beban kerja

--- app/components/abk/views.py
+++ app/components/abk/views.py
@@ -1,11 +1,11 @@
 from flask import flash, redirect, render_template, url_for, request, session, app
 from flask_login import login_required
 from . import abk
-from ...models import Jabatan, Pegawai
+from ...models import Jabatan, Pegawai, Ikhtisar_Jabatan
 from ... import db
 
 
-@abk.route('/form-b', methods=['GET', 'POST'])
+@abk.route('/analisis-beban-kerja', methods=['GET', 'POST'])
 @login_required
 def form_b():
     # list = enumerate(Jabatan.query.all(), start=1)
@@ -13,9 +13,61 @@ def form_b():
     list = []
     for j in jabatan:
         i = {}
+        
+        #Nama Jabatan
         i['name'] = j.name
+        
+        # Jumlah beban kerja jabatan
+        total = 0
+        uraian = Ikhtisar_Jabatan.query.filter(Ikhtisar_Jabatan.jabatan_id == j.id).all()
+        if uraian:
+            for u in uraian:
+                if u.volume and u.waktu:
+                    beban = u.volume * u.waktu
+                else:
+                    beban = 0
+                total = total + beban
+                
+        hitung = total/60
+        i['beban'] = round(hitung, 2)
+        
+        # Kebutuhan Pegawai
+        kebutuhan = round(hitung/1200)
+        i['kebutuhan'] = kebutuhan
+        
+        # Jumlah Pegawai yang ada
         size = len(j.pegawai)
         i['count'] = size
+        
+        # +/-
+        qty = kebutuhan - size
+        i['plusminus'] = qty
+        
+        # Efisiensi
+        if hitung and size :
+            ef = hitung / (size*1200)
+        else:
+            ef = 0
+        i['efisiensi'] = ef
+        
+        # Prestasi dan Keterangan
+        if ef < 0.5 :
+            i['prestasi'] = 'E'
+            i['ket'] = 'Kurang'
+        elif ef <= 0.69:
+            i['prestasi'] = 'D'
+            i['ket'] = 'Sedang'
+        elif ef <= 0.89:
+            i['prestasi'] = 'C'
+            i['ket'] = 'Cukup'
+        elif ef <= 1:
+            i['prestasi'] = 'B'
+            i['ket'] = 'Baik'
+        elif ef > 1:
+            i['prestasi'] = 'A'
+            i['ket'] = 'Sangat Baik'
+            
+        # Masukkan list baru
         list.append(i)
         
     list = enumerate(list, start=1)
